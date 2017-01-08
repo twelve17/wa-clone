2e48e04da2930706e22f169bad02a92b9304c04a
twelve17 <spam@twelve17.com>
2017-01-08 23:41:53 +0000

    redis setup and test

--- src/api-server.js
+++ src/api-server.js
@@ -1,11 +1,26 @@
-import express from 'express';
+import express from "express";
+import {setupRedis, redisClient} from "./redis-client";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello React Class, from Express.js!');
+app.get("/", (req, res) => {
+  res.send("Hello React Class, from Express.js!");
 });
+
+app.get("/redis-test", (req, res) => {
+  redisClient.incr("inc-test", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.send("Error connecting to redis");
+    }
+    else {
+      res.send(`New incremented value: ${result}`);
+    }
+  });
+});
+
+setupRedis();
 
 
 app.listen(port, () => {
